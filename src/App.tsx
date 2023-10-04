61c55bb3a44a587d035d6e2a05a4eb993fc186c4
Artur <alimayko91@gmail.com>
2023-10-04 11:11:39 +0300

    complete lesson 1,2,3,4

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from './components/Accordion';
+import {Rating} from "./components/Rating/Rating";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(props: any) {
+    //  что то полезное делает
+    //обязана вернуть JSX
+    console.log("App rendering")
+    return (
+        <div className="App">
+            <PageTitle title={"This is App component"}/>
+            <PageTitle title={"Users page"}/>
+
+            Article 1
+            <img src={"https://cdn.iconscout.com/icon/free/png-256/free-webstorm-3629742-3030793.png"}/>
+            <Rating value={0}/>
+            <Accordion titleValue={"This is first title"} collapsed={true}/>
+            <Accordion titleValue={"This is second title"} collapsed={false}/>
+            Article 2
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+        </div>
+    );
 }
+
+type PageTitlePropsType = {
+    title : string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+    console.log("AppTitle rendering")
+    return <h1>{props.title}</h1>
+}
+
+
 
 export default App;
