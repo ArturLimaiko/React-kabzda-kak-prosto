67ad2fdca070838a29e0e22cf2fed30b9f53b483
Artur <alimayko91@gmail.com>
2023-10-31 19:51:14 +0300

    lesson 8 callback - refactor UncontrolledRating

--- src/components/Rating/UncontrolledRating.tsx
+++ src/components/Rating/UncontrolledRating.tsx
@@ -1,30 +1,38 @@
 import React, {useState} from "react";
 
+
 export function UncontrolledRating() {
     //ЕСЛИ В КОМПОНЕНТЕ ЕСТЬ useState И ЕСЛИ ЭТОТ СТЕЙТ МЕНЯЕТСЯ
     // ТО ПЕРЕРИСОВЫВАЕТСЯ ТОЛЬКО ЭТОТ КОМПОНЕНТ!
     //создаем локальный стейт
-    //изначальное значение рейтинга 0
-let [rating , setRating] = useState( 0)
+    //изначальное значение рейтинга 4
+    let [rating, setRating] = useState(0)
 
     return (
         <div>
             {/*rating у каждой звезды свой и мы храним его в локальном стейте
             при клике на кнопку запустится наша функция setRating() и передаем значение 1 и тд*/}
-            <Star selected={rating > 0}/><button onClick={() => {setRating(1)}}>1</button>
-            <Star selected={rating > 1}/><button onClick={() => {setRating(2)}}>2</button>
-            <Star selected={rating > 2}/><button onClick={() => {setRating(3)}}>3</button>
-            <Star selected={rating > 3}/><button onClick={() => {setRating(4)}}>4</button>
-            <Star selected={rating > 4}/><button onClick={() => {setRating(5)}}>5</button>
+            <Star selected={rating > 0} setRating={setRating} rating={1}/>
+            <Star selected={rating > 1} setRating={setRating} rating={2}/>
+            <Star selected={rating > 2} setRating={setRating} rating={3}/>
+            <Star selected={rating > 3} setRating={setRating} rating={4}/>
+            <Star selected={rating > 4} setRating={setRating} rating={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
     selected: boolean
+    rating: 1 | 2 | 3 | 4 | 5
+    setRating: (rating: 1 | 2 | 3 | 4 | 5) => void
 }
 
 function Star(props: StarPropsType) {
-    console.log("Star rendering")
-   return props.selected ? <span><b>  -- Star --  </b></span> : <span>  -- Star --  </span>
+    // return props.selected ? <span><b>  -- Star --  </b></span> : <span>  -- Star --  </span>
+    // ниже еще мощнее зарефакторил
+    return <span onClick={() => {
+        props.setRating(props.rating)
+    }}>
+            {props.selected ? <b> -- Star -- </b> : ' -- Star -- '}
+            </span>
 }
